9d2e3eb870cf7c1a35c1f824153d8628115a6121
Matteo Magnani <magnani.matteo95@gmail.com>
2016-02-08 22:19:17 +0100

    test position conversion from model to view

--- src/control/GameThread.java
+++ src/control/GameThread.java
@@ -1,5 +1,8 @@
 package control;
 
+import java.util.List;
+import java.util.Map;
+
 import model.Model;
 import view.ViewController;
 
@@ -15,7 +18,14 @@ public class GameThread extends Thread {
 	public void run() {
 		while(true){
 			this.model.updateArena();
-			this.view.updateScene(this.model.getState());
+			Map<Entities, List<Pair<Integer, Position>>> map = this.model.getState();
+			map.entrySet().stream().map(e -> e.getValue()).forEach(e -> {
+                            e.stream().map(c -> c.getY().getPoint()).forEach(c -> {
+                                c.setX(c.getX()*2);
+                                c.setY(c.getY()*-2);
+                            });;
+                        });
+			this.view.updateScene(map);
 			try {
 				Thread.sleep(30L);
 			} catch (InterruptedException e) {
