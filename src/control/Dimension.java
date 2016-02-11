e3cc43b33e09aa392d08d3251afff22a38fdd546
Matteo Magnani <magnani.matteo95@gmail.com>
2016-02-11 12:02:42 +0100

    equals and hash code generated for some classes

--- src/control/Dimension.java
+++ src/control/Dimension.java
@@ -1,7 +1,7 @@
 package control;
 
 public class Dimension {
-    
+
     private final int width;
     private final int height;
     
@@ -18,4 +18,28 @@ public class Dimension {
         return this.height;
     }
     
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + height;
+        result = prime * result + width;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dimension other = (Dimension) obj;
+        if (height != other.height)
+            return false;
+        if (width != other.width)
+            return false;
+        return true;
+    }
 }
--- src/control/Position.java
+++ src/control/Position.java
@@ -21,5 +21,37 @@ public class Position {
     public Dimension getDimension() {
         return new Dimension(this.dimension.getWidth(), this.dimension.getHeight());
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((dimension == null) ? 0 : dimension.hashCode());
+        result = prime * result + ((point == null) ? 0 : point.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Position other = (Position) obj;
+        if (dimension == null) {
+            if (other.dimension != null)
+                return false;
+        } else if (!dimension.equals(other.dimension))
+            return false;
+        if (point == null) {
+            if (other.point != null)
+                return false;
+        } else if (!point.equals(other.point))
+            return false;
+        return true;
+    }
+    
     
 }
