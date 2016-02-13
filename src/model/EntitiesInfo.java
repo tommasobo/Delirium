923620f0824b4c6dffab33018b99dde3ba22894c
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2016-02-13 12:05:53 +0100

    Changed EntitiesInfo and added the interfaces

---
+++ src/model/EntitiesInfo.java
@@ -0,0 +1,23 @@
+package model;
+
+public interface EntitiesInfo {
+
+    int getCode();
+
+    int getLife();
+
+    LifeManager getLifemanager();
+
+    MovementTypes getMovementTypes();
+
+    Position getPosition();
+
+    Bounds getBounds();
+
+    int getSpeed();
+
+    boolean isCanFly();
+
+    int getContactDamage();
+
+}
