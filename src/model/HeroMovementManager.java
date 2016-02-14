1175a3c3bad2d33f7532a72c0c9c0874266970ad
Matteo Magnani <matteo.magnani18@studio.unibo.it>
2016-02-14 02:58:59 +0100

    jump ok

--- src/model/HeroMovementManager.java
+++ src/model/HeroMovementManager.java
@@ -11,39 +11,34 @@ public class HeroMovementManager extends DinamicMovementManager{
 
     @Override
     public Position getNextMove() {
-        Actions action = determinateAction(this.getDirection());
+    	Position newPosition = this.getPosition();
+    	Actions action = determinateAction(newPosition.getDirection());
+    	
         //boolean checkUp = this.getDirection().getFunction().apply(this.getPosition().getPoint(), this.getPosition().getSpeed()).getY() <= this.getBounds().getMaxY() + this.getPosition().getDimension().getHeight();
         boolean checkDx = action.getFunction().apply(this.getPosition().getPoint(), this.getSpeed()).getX() <= this.getBounds().getMaxX();
         boolean checkSx = action.getFunction().apply(this.getPosition().getPoint(), this.getSpeed()).getX() >= this.getBounds().getMinX();
         //boolean checkDown = this.getDirection().getFunction().apply(this.getPosition().getPoint(), this.getPosition().getSpeed()).getY() >= this.getBounds().getMinY() - this.getPosition().getDimension().getHeight();
         
         //MAGNANI PART BEGIN
-        
-        if (checkDx && checkSx && this.getDirection() != Directions.UP && this.getDirection() != Directions.DOWN) {
-            this.setPosition(action.getFunction().apply(this.getPosition().getPoint(), this.getSpeed()), this.getDirection());
-        } else if (this.getDirection() == Directions.UP) {
-            this.onJump = true;
-        }
 
-        Position newPosition;
         
+        //TODO eventualmente aumetare il salto ed applicare comunque la gravità, a livello astratto èpiù logico forse
         if(!onJump) {
-            newPosition = applyGravity(this.getPosition());
-        } else {
-            newPosition = this.getPosition();
-            
+            newPosition = applyGravity(newPosition);
         }
         
-        if(newPosition.getPoint().getY() == this.getPosition().getPoint().getY() && !onJump ) {
+        if(newPosition.getPoint().getY() == this.getPosition().getPoint().getY() && !onJump && this.getDirection() == Directions.UP) {
             time = 0;
-            if(this.getDirection() == Directions.UP) {
-                this.onJump = true;
-            }
+            this.onJump = true;
+        } else if (newPosition.getPoint().getY() < this.getPosition().getPoint().getY()) {
+        	newPosition.setDirection(Directions.DOWN);
         }
         
         if (onJump) {
             if(time < 60) {
                 newPosition.setPoint(Actions.UP.getFunction().apply(newPosition.getPoint(), 5));
+                newPosition.setDirection(Directions.UP);
+                //TODO metti controllo bounds salto
                 /*if (checkUp) {
                     newPosition.setPoint(ModelDirections.UP.getFunction().apply(newPosition.getPoint(), 1));
                 } else {
@@ -52,9 +47,15 @@ public class HeroMovementManager extends DinamicMovementManager{
                 time++;
             } else {
                 onJump = false;
-                newPosition.setDirection(Directions.DOWN);
             }
         }
+        
+        //applico le funzioni dopo il calcolo dell'eventuale salto o caduta in modo da poter settare la reale direzione del personaggio 
+        if (checkDx && checkSx && (action == Actions.LEFT || action == Actions.RIGHT)) {
+        	newPosition.setPoint(action.getFunction().apply(newPosition.getPoint(), this.getSpeed()));
+        	newPosition.setDirection(this.getDirection());
+        }
+        
         // MAGNANI PART FINISH
         
         return newPosition;
