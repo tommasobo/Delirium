eee1df438c433d878ae9af020404cdfe89afc180
Matteo Magnani <matteo.magnani18@studio.unibo.it>
2016-02-24 14:58:49 +0100

    buttons names added

--- src/control/Buttons.java
+++ src/control/Buttons.java
@@ -1,13 +1,25 @@
 package control;
 
 public enum Buttons {
-	NEWGAME(ViewEvents.LEVEL1), MAINMENU(ViewEvents.BACKTOMAINMENU), EXIT(ViewEvents.EXIT), NEXTLEVEL(ViewEvents.NEXTLEVEL);
-	private final ViewEvents event;
-	private Buttons(ViewEvents event) {
-		this.event = event;
-	}
-	public ViewEvents getEvent() {
-		return event;
-	}
-	
+    NEWGAME("New Game", ViewEvents.LEVEL1),
+    MAINMENU("Main Menu", ViewEvents.BACKTOMAINMENU),
+    EXIT("Exit", ViewEvents.EXIT),
+    NEXTLEVEL("Next Level", ViewEvents.NEXTLEVEL);
+    
+    private final ViewEvents event;
+    private final String name;
+
+    private Buttons(String name, ViewEvents event) {
+        this.event = event;
+        this.name = name;
+    }
+
+    public ViewEvents getEvent() {
+        return event;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
